92443bdd851119b170d70a7e2324f02c870cb2c6
tm-clockworks <trevor.moir@yahoo.ca>
2023-06-15 11:56:04 -0400

    Update from JSON server to static products v2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,17 @@
-import { useState } from 'react'
-import { useFetch } from "../hooks/useFetch";
+// import { useState } from 'react'
+// import { useFetch } from "../hooks/useFetch";
 import { useTitle } from "../hooks/useTitle";
 
 import { ProductCard } from "../components/ProductCard"
 
-export const Home = ({cart, setCart, cartItem, setCartItem}) => {
+export const Home = ({products, cart, setCart, cartItem, setCartItem}) => {
     useTitle("Home");
     //use if using json server
     //const [url, setUrl] = useState("http://localhost:8000/products");
     //const { data: products, loading, error } = useFetch(url, { contect: 'ABC'});
 
-    const products = [
-        {"id": 1, "name": "Sony Wh-Ch510 Bluetooth Wireless", "price": 149, "image": "/assets/images/1001.png"},
-        {"id": 2, "name": "boAt Rockerz 450", "price": 49, "image": "/assets/images/1002.png"},
-        {"id": 3, "name": "JBL Tune 760NC", "price": 179, "image": "/assets/images/1003.png"},
-        {"id": 4, "name": "Logitech H111 Wired", "price": 39, "image": "/assets/images/1004.png"},
-        {"id": 5, "name": "APPLE Airpods Max Bluetooth Headset", "price": 199, "image": "/assets/images/1005.png"},
-        {"id": 6, "name": "ZEBRONICS Zeb-Thunder Wired", "price": 29, "image": "/assets/images/1006.png"}
-      ]
-
-  return (
+    console.log(products)
+;  return (
     <main>
         <section className="products">
             {/* <div className="filter"> */}
@@ -27,8 +19,8 @@ export const Home = ({cart, setCart, cartItem, setCartItem}) => {
                 {/* <button className="all" onClick={() => setUrl("http://localhost:8000/products")}>All</button>
                 <button className="onlystock" onClick={() => setUrl("http://localhost:8000/products?in_stock=true")}>In Stock Only</button>
             </div> */}
-            { loading && <p>{error}</p>}
-            { error && <p className="loading">Loading products...</p>}
+            {/* { loading && <p>{error}</p>}
+            { error && <p className="loading">Loading products...</p>} */}
             {products && products.map((product) => (
                 <ProductCard product={product}  key={product.id} cart={cart} setCart={setCart} cartItem={cartItem} setCartItem={setCartItem} />
             ))}
--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,35 +1,36 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useTitle } from "../hooks/useTitle";
-import { useFetch } from "../hooks/useFetch";
+// import { useTitle } from "../hooks/useTitle";
+// import { useFetch } from "../hooks/useFetch";
 
-export const ProductDetail = () => {
+export const ProductDetail = ({products}) => {
   const params = useParams();
   console.log(params.id);
     //use if using json server
     //const [url, setUrl] = useState(`http://localhost:8000/products/${params.id}`);
     //const { data: products, loading, error } = useFetch(url, { contect: 'ABC'});
-    
+    const product = products.find(o => parseInt(o.id) === parseInt(params.id));
+
     //useTitle(products.name);
     useEffect(() => {
-      document.title = products ? `${products.name} | Shopping Cart` : "Shopping Cart";
-    },[products]);
+      document.title = products ? `${product.name} | Shopping Cart` : "Shopping Cart";
+    },[product]);
 
     return (
       <main>
-        { error && <p>{error}</p>}
-        { loading && <p className="loading">Loading products...</p>}
-        { products &&
+        {/* { error && <p>{error}</p>}
+        { loading && <p className="loading">Loading products...</p>} */}
+        { product &&
         <section className="flex justify-around flex-wrap py-5">
           <div className="max-w-sm">
-            <img className="rounded" src={products.image} alt={products.name} />
+            <img className="rounded" src={product.image} alt={product.name} />
           </div>
           <div className="max-w-2xl text-gray-700 text-lg dark:text-white">
-            <h1 className="text-4xl font-bold my-3 text-center lg:text-left">{products.name}</h1>
+            <h1 className="text-4xl font-bold my-3 text-center lg:text-left">{product.name}</h1>
 
             <p className="my-4">
               <span className="mr-2 font-bold">Price:</span>
-              <span>${products.price}</span>
+              <span>${product.price}</span>
             </p>
           </div>
         </section>
